6ee7d2c02fb1e573d1e95cb75dcdcb3b5a7765ff
Juha <viderus@gmail.com>
2023-02-20 14:13:57 +0200

    added sql scripts to create and alter data in the three required tables

---
+++ 05_insertAssistants.sql
@@ -0,0 +1,9 @@
+INSERT INTO assistant (name, superheroId)
+VALUES ('Pekka Kivimäki', 1);
+
+INSERT INTO assistant (name, superheroId)
+VALUES ('Alfred Hitchcock', 2);
+
+INSERT INTO assistant (name, superheroId)
+VALUES ('Newton', 3);
+
---
+++ 06_powers.sql
@@ -0,0 +1,14 @@
+INSERT INTO power (name, description)
+VALUES ('Super strength', 'Can lift heavy objects'),
+('Super speed', 'Can run fast'),
+('Super sight', 'Can see far'),
+('Invisibility', 'Can become invisible');
+
+INSERT INTO superheroPower (superheroId, powerId)
+VALUES (1, 1),
+(1, 2),
+(1, 3),
+(2, 1),
+(2, 2),
+(2, 4),
+(3, 1);
