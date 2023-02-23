fba906a53e35c8f2d561c7b69f4a75f5e3339f6b
Juha <viderus@gmail.com>
2023-02-23 11:47:44 +0200

    added 4 queries to CustomerImpl -> ListAll, List all with offset, get by id and get by name

--- src/main/java/models/Customer.java
+++ src/main/java/models/Customer.java
@@ -1,7 +1,7 @@
 package models;
 
 public class Customer {
-    private String customer_id;
+    private int customer_id;
     private String first_name;
     private String last_name;
     private String country;
@@ -9,7 +9,7 @@ public class Customer {
     private String phone;
     private String email;
 
-    public Customer(String customer_id,
+    public Customer(int customer_id,
                     String first_name,
                     String last_name,
                     String country,
@@ -49,11 +49,11 @@ public class Customer {
         this.postal_code = postal_code;
     }
 
-    public String getCustomer_id() {
+    public Integer getCustomer_id() {
         return customer_id;
     }
 
-    public void setCustomer_id(String customer_id) {
+    public void setCustomer_id(Integer customer_id) {
         this.customer_id = customer_id;
     }
 
