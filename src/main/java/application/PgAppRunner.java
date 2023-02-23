9592c07efe3cd7cc8a2a90224690e87ef820bfab
Juha <viderus@gmail.com>
2023-02-23 15:00:46 +0200

    clean-up: removed the unused classes

--- src/main/java/application/PgAppRunner.java
+++ src/main/java/application/PgAppRunner.java
@@ -6,19 +6,17 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 import repositories.CustomerRepository;
+
 @Component
 @ComponentScan("repositories")
 public class PgAppRunner implements ApplicationRunner {
-    private final CustomerRepository customerRepository;
 
     @Autowired
     public PgAppRunner(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        customerRepository.test();
-        customerRepository.listAll();
+
     }
 }
--- src/main/java/dataRequests/ReadCustomerByName.java
+++
@@ -1,6 +0,0 @@
-package dataRequests;
-
-public class ReadCustomerByName {
-
-
-}
